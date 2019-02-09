9f5161a275b62d2bd44c5e6ccf3c5ad519b91174
karuan <kruan@andrew.cmu.edu>
2019-02-09 11:46:45 -0500

    temp

--- src/CONST.py
+++ src/CONST.py
@@ -9,10 +9,20 @@ GPIO_INPUT_PINS = [
   10
 ]
 
-GUITAR_SOUNDS_DIR = 'guitar-electric/'
+GUITAR_SOUNDS_DIR = 'guitar-electric/audacityTracks'
 
 GUITAR_NOTES = [
   'A2.ogg',
-  'Cs2.ogg',
-  'A3.ogg'
+  'Bb2.ogg',
+   'B2.ogg',
+   'C3.ogg',
+   'Cs3.ogg',
+   'D3.ogg',
+   'Ds3.ogg',
+   'E3.ogg',
+   'F3.ogg',
+   'Fs3.ogg',
+   'G3.ogg',
+   'Ab3.ogg',
+   'A3.ogg'   
 ]
--- src/sound.py
+++ src/sound.py
@@ -4,6 +4,7 @@ Main file for controlling button logic and registering sounds.
 import pygame
 from time import sleep
 from CONST import *
+"""
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 # GPIO setup
@@ -13,7 +14,7 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 for pin in GPIO_INPUT_PINS:
   # Pull down resistor for buttons
   GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+"""
 # Pygame sound setup
 pygame.mixer.init() #turn all of pygame on.
 pygame.mixer.set_num_channels(NUM_CHANNELS)  # default is 8
@@ -36,9 +37,15 @@ for s in GUITAR_NOTES:
   sounds.append(pygame.mixer.Sound(GUITAR_SOUNDS_DIR + s))
 
 while True: # Run forever
+  sleep(0.1)
+  for i in range(len(sounds)):
+      playSound(sounds[i]) 
+      sleep(1.0)
+  """   
   for i in range(len(GPIO_INPUT_PINS)):
     inputPin = GPIO_INPUT_PINS[i]
 
     if GPIO.input(inputPin) == GPIO.HIGH:
       print('Hit string {0}'.format(i))
       playSound(sounds[i])
+  """
