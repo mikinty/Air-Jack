db88e4674b5c76768b3c05935a7e91a5be7d9a9a
mikinty <myou@andrew.cmu.edu>
2019-02-09 03:31:58 -0500

    more sound

--- src/sound.py
+++ src/sound.py
@@ -6,12 +6,19 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-
 pygame.mixer.init() #turn all of pygame on.
+pygame.mixer.set_num_channels(10)  # default is 8
+
+currChannel = 0
+
+def playSound(sound):
+  pygame.mixer.Channel(currChannel).play(sound)
+  currChannel += 1
+
 
 effect = pygame.mixer.Sound('guitar-electric/A2.ogg')
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
-        effect.play()
+        playSound(effect)
